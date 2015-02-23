28fac6d758f6f13e727ce74fbad1daf829c7f4be
Michael Schlottke <m.schlottke@fz-juelich.de>
2015-02-23 16:44:00 +0100

    Make scripts executable; add shebang to first line

--- before.sh
+++ before.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 # Load configuration data
 . config
 
 # Create new RPM database directory
+set -x
 mkdir -p $RPMDBPATH
--- inst2.sh
+++ inst2.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Load configuration data
 . config
 
