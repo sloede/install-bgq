f6308d96bf9be8daed861d6426285c7494f47a81
Michael Schlottke <m.schlottke@fz-juelich.de>
2015-03-20 10:12:43 +0100

    Remove obsolete files

--- before.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Load configuration data
-. config
-
-# Create new RPM database directory
-set -x
-mkdir -p $RPMDBPATH
--- inst0.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Load configuration data
-. config
-
-# Install
-set -x
-rpm -Uhv --dbpath $RPMDBPATH --prefix $PREFIX_BASE/$VERSION $RPMDIR/vpkg-bin-sh-*.ppc64.rpm
